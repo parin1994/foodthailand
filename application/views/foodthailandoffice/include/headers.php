21a359991cbe5ae89d4bc50b875849b5a35fe208
Parin <Parin.eye1994@gmail.com>
2021-01-18 23:13:09 +0700

    include

---
+++ application/views/foodthailandoffice/include/headers.php
@@ -0,0 +1,14 @@
+<header class="header black-bg">
+            <div class="sidebar-toggle-box">
+                <div class="fa fa-bars tooltips" data-placement="right"></div>
+            </div>
+            <!--logo start-->
+            <a class="logo"><b style="font-size:18px">FOOD<span>THAILAND</span></b></a>
+            <!--logo end-->
+          
+            <div class="top-menu">
+                <ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="<?php echo base_url('foodthailandoffice/login/logout')?>">ออกจากระบบ</a></li>
+                </ul>
+            </div>
+        </header>
