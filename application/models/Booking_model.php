dc5b9404de4e53f3ad3a7a0ef860ffa2065afb21
Parin <Parin.eye1994@gmail.com>
2021-01-24 21:37:52 +0700

    no message

--- application/models/Booking_model.php
+++ application/models/Booking_model.php
@@ -39,7 +39,7 @@ class booking_model extends CI_Model {
         return $query->result();
     }
     public function order($id_booking){
-        $sql = "SELECT `order`.id_order,food.name_food,food.price,`order`.qty,`order`.detial FROM `order` inner join food on `order`.id_food = food.id_food where `order`.id_booking = $id_booking";
+        $sql = "SELECT `order`.id_order,food.name_food,food.price,`order`.qty,`order`.detail FROM `order` inner join food on `order`.id_food = food.id_food where `order`.id_booking = $id_booking";
         $query = $this->db->query($sql);
         return $query->result();
     }
