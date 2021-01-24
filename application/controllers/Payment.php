ad7af29774214e60c5bf452aa9ed5f9c7d630f0b
Parin <Parin.eye1994@gmail.com>
2021-01-24 15:50:00 +0700

    no message

--- application/controllers/Payment.php
+++ application/controllers/Payment.php
@@ -38,8 +38,7 @@ class Payment extends CI_Controller
             $ftp_config['debug']    = TRUE;
 
             //Connect to the remote server
-            $this->ftp->ftp_connect($ftp_config);
-            // $this->Ftp->connect($ftp_config);
+            $this->ftp->connect($ftp_config);
             $this->ftp->upload($_FILES['img']['tmp_name'], "/public_html/assets/food/" . $_FILES['img']['name'], "ascii", 0775);
 
             //Close FTP connection
