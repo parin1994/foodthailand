d63758d0e438c04500084ee33997c96208d2cdc9
Parin <Parin.eye1994@gmail.com>
2021-01-25 10:40:37 +0700

    edit button save

--- application/views/foodthailandoffice/service/form.php
+++ application/views/foodthailandoffice/service/form.php
@@ -63,7 +63,7 @@
                             </div>
 
                             
-                            <form role="form" action="<?php echo base_url('foodthailandoffice/service/' . $method . '/' . $con->id_food) ?>" method="post"  enctype="multipart/form-data">
+                            <form role="form" action="<?php echo base_url('foodthailandoffice/services/' . $method . '/' . $con->id_food) ?>" method="post"  enctype="multipart/form-data">
                             
                             <div class="box-body">
                                 <div class="form-group">
@@ -78,8 +78,8 @@
 
                                 </div>
                                 <div class="box-footer">
-                                    <button href="<?php echo base_url('foodthailandoffice/service/services') ?>" class="btn btn-primary" type="submit" name="btnsave" id="btnSave ">Save</button>
-                                    <a href="<?php echo base_url('foodthailandoffice/service/index') ?>" class="btn btn-danger">Cancel</a>
+                                    <button href="<?php echo base_url('foodthailandoffice/services/index') ?>" class="btn btn-primary" type="submit" name="btnsave" id="btnSave ">Save</button>
+                                    <a href="<?php echo base_url('foodthailandoffice/services/index') ?>" class="btn btn-danger">Cancel</a>
                                     <input type="hidden" name="method" value="<?php echo $method ?>">
                                     <input type="hidden" name="id" value="">
                                 </div>
