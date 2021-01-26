a776975e88c51898b97ceed083fb56c3ae7da181
Parin <Parin.eye1994@gmail.com>
2021-01-26 19:51:20 +0700

    no message

--- application/controllers/Curl.php
+++ application/controllers/Curl.php
@@ -10,7 +10,7 @@ class Curl extends CI_Controller {
     {
         $this->booking_model->message_curl($id_booking);
         $url = 'https://api.line.me/v2/bot/message/push';
-        $authorization = "jiwlVxvbHqfjsWu3YfY69ND2UHns4wbd/Yu6UV5p7Y+EpqBaUCnJD9Oo6YXVdyQQddswlvGYf2zvDgFwEWYiIW88zL09X0V9QezXwuVoEoYw6c94OlWJ6DC0hxnZXJrZAwSg3LJxV6m+8mEBBEuFmwdB04t89/1O/w1cDnyilFU=";
+        $authorization = "LAVPut6p2Seyw/s97j1L3cMOH/rvoUvXvJluohkrnIkWtdh2TO8cS2n1LQFpofSsBBOkkUfKKjdOF5GNn2KOayOCtcMPd+mqrThpGwGr8Ss1b+p/hB5nodrQHqFA5gb4X2mhOBEg1fpTtWFubt9hfgdB04t89/1O/w1cDnyilFU=";
         $post = '{
             "to": "'.$userid.'",
             "messages": [
