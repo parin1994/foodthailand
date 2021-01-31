dc44481f5f92f383e9af44a8b266ee57277d7ee9
Parin <Parin.eye1994@gmail.com>
2021-01-31 21:57:18 +0700

    no message

--- application/views/booking.php
+++ application/views/booking.php
@@ -39,9 +39,9 @@
                     <tr id="tr"></tr>
                 </tbody>
         </div>
-        <button type="button" name="save" id="save" class="btn btn-primary" hidden>Save</button>
+        
     </div>
-
+    <button type="button" name="save" id="save" class="btn btn-primary" hidden>Save</button>
     </div>
 </body>
 
@@ -75,7 +75,6 @@
                         '</tr>';
 
                     $('#tr').after(html);
-                    $('#save').show();
                     }
                 });
             } else {
