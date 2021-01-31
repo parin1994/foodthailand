7704865041485bbdc3da704f84c20776d474458a
Parin <Parin.eye1994@gmail.com>
2021-01-31 19:48:31 +0700

    no message

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -39,7 +39,50 @@ class Booking extends CI_Controller
         $this->session->set_userdata('cart', $data);
         $cart = $this->session->userdata('cart');
         print_r($cart);
-        echo $this->view();
+        
+        $output = '';
+        $output .= '
+        <h3>Shopping Cart</h3><br />
+        <div class="table-responsive">
+        <div align="right">
+        <button type="button" id="clear_cart" class="btn btn-warning">Clear Cart</button>
+        </div>
+        <br />
+        <table class="table table-bordered">
+        <tr>
+        <th width="40%">Name</th>
+        <th width="15%">Quantity</th>
+        <th width="15%">Price</th>
+        <th width="15%">Total</th>
+        <th width="15%">Action</th>
+        </tr>
+        ';
+        $count = 0;
+        foreach ($cart as $items) {
+            $count++;
+            $output .= '
+        <tr> 
+            <td>' . $items[0]->name_food . '</td>
+            <td>' . $items[1] . '</td>
+            <td>' . $items[2] . '</td>
+            <td>' . $items[3] . '</td>
+            <td><button type="button" name="remove" class="btn btn-danger btn-xs remove_inventory" id="' . $items["rowid"] . '">Remove</button></td>
+            </tr>
+        ';
+        }
+        $output .= '
+             <tr>
+            <td colspan="4" align="right">Total</td>
+            <td>' . $this->cart->total() . '</td>
+            </tr>
+        </table>
+
+  </div>
+  ';
+        if ($count == 0) {
+            $output = '<h3 align="center">Cart is Empty</h3>';
+        }
+        return $output;
     }
     public function view()
     {
